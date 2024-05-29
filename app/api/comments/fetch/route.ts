31722b92f7f400dce758710acb030ee24d202840
Dylan J <dylan@suhd00d.co>
2024-05-29 08:31:32 -0400

    Update route.ts

--- app/api/comments/fetch/route.ts
+++ app/api/comments/fetch/route.ts
@@ -1,6 +1,8 @@
 import "@/db/connect";
 import dataModel from "@/db/model";
 
+export const revalidate = 0;
+
 export async function GET(req: Request) {
   const comments = await dataModel.find().sort({ _id: -1 }).lean();
 
