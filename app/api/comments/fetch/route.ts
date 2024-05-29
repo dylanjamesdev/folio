9e9fd053ff1a15e768d0e877d73b80e832fa3976
devfxx <fxx@kty.lol>
2024-05-29 15:22:19 +0300

    chore: Refactor fetch route to use lean() performance boost

--- app/api/comments/fetch/route.ts
+++ app/api/comments/fetch/route.ts
@@ -2,7 +2,7 @@ import "@/db/connect";
 import dataModel from "@/db/model";
 
 export async function GET(req: Request) {
-  let comments = await dataModel.find({}).sort({ _id: -1 });
+  const comments = await dataModel.find().sort({ _id: -1 }).lean();
 
   return new Response(JSON.stringify(comments), {
     status: 200,
